48abc5467698720277faf0813ab6e9934d0418e8
William Librata <william.librata@gmail.com>
2017-05-15 03:05:00 +1000

    Add deliverables

--- deliverables/schools_by_locality.sql
+++ deliverables/schools_by_locality.sql
@@ -5,7 +5,7 @@ CREATE TABLE schools_by_locality AS
     SELECT lc_ply_pid AS locality_id, vic_loca_2 AS locality_name, count(*) AS schools
     FROM locality l
     INNER JOIN school s
-        ON ST_Intersects(l.boundary, s.location) = True
+        ON ST_Intersects(l.geom, s.location) = True
     GROUP BY lc_ply_pid, vic_loca_2
 );
 
@@ -14,5 +14,13 @@ SELECT lc_ply_pid AS locality_id, vic_loca_2 AS locality_name, 0 AS schools
 FROM locality l
 WHERE NOT EXISTS
 (
-    SELECT 1 FROM schools_by_locality sbl WHERE l.locality_id = sbl.lc_ply_pid
+    SELECT 1 FROM schools_by_locality sbl WHERE l.lc_ply_pid = sbl.locality_id
 );
+
+ALTER TABLE locality DROP COLUMN IF EXISTS schools_count;
+ALTER TABLE locality ADD schools_count INT;
+UPDATE locality l
+SET schools_count = sbl.schools
+FROM schools_by_locality sbl
+WHERE l.lc_ply_pid = sbl.locality_id;
+
